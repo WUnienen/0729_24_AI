3718427e044b209c5909dbcbdff094149e3442bb
WUnienen <hanaesakuko@gmail.com>
2024-08-23 14:26:30 +0000

    7

---
+++ lesson7/lesson7_1.py
@@ -0,0 +1,24 @@
+#猜數字遊戲
+import random
+import pyinputplus as pyip
+
+min=1
+max=100
+c=0
+t=random.randint(1,100)
+print("===========猜數字==========")
+while True:
+    c+=1
+    k=pyip.inputInt(f"猜數字範圍{min}-{max}:",min=min,max=max)
+    if k==t:
+        print(f"對了 答案是{k}")
+        print(f"您猜了{c}次")
+        break
+    elif k>t:
+        print("在小一點")
+        max=k-1
+    elif k<t:
+        print("再大一點")
+        min=k+1
+    print(f"您已經猜了{c}次")
+print("應用程式結束")
